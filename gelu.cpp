efc4c05de2835ab8e7e980729b40dba34e7409b4
root <root@DESKTOP-H1KQG38>
2024-06-04 05:53:46 +0800

    Build gelu.cpp with no prob for the first peek
    like title says, it lgtm, because we have not test yet, so
    it remains not sure.

--- gelu.cpp
+++ gelu.cpp
@@ -1,8 +1,14 @@
 #include <cmath>
 #define GELU_SCALING_FACTOR sqrtf(2.0f / M_PI)
-
+float sigmoid(float x) { return 1.0 / (1.0 + exp(-x)); }
 void gelu_forward_cpu(float* out, const float* inp, int N) {
-    // vvvvv Put your code here vvvvv
-
-    // ^^^^^ Put your code here ^^^^^
+    
+    for (int i = 0; i < N; ++i) {
+        x = inp[i];//not sure if just use inp[i]will make it slower
+        
+        //first version
+        out[i] = 0.5f * x * (tanhf(GELU_SCALING_FACTOR * (x *(1 + 0.044715 * x * x)));//tanhf, thef means float not double
+        //second version
+        //out[i] = x * sigmoid(1.702 * x)
+    }
 }
