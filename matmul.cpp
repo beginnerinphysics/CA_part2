4e69a05e4fda7eaafb18a5b4289c10d98040f50c
beginnerinphysics <eroha90021@gmail.com>
2024-06-08 19:00:36 +0000

    Build matmul.cpp with lots uncertain
    I think the block is used for better locality, and bias is in
    dimension C, so I make input times multiply weight with inp[i][k]
    and weight[k][j] which I am not sure it's be weight[k][j]or jk
    and not sure bias length is C or T

--- matmul.cpp
+++ matmul.cpp
@@ -4,11 +4,23 @@
 #include <stdio.h>
 #define block_t 4
 #define block_oc 4
-#define block_c 4
-void matmul_forward_cpu(float* out,
+#define block_c 4//初版完成之後有時間需要回來研究修改此block, 已達到更好的locality
+using namespace std;
+void matmul_forward_cpu(float* out, //這裡的matmul單純是把inp和W, b相乘相加線性投射而已, y=XW^T+b, X的矩陣是T*C, W的空間似乎是OC*C, b似乎是OC的空間, 同個token的轉換會使用同個值的b
                     const float* inp, const float* weight, const float* bias,
                     int T, int C, int OC) {
-    // vvvvv Put your code here vvvvv
-
-    // ^^^^^ Put your code here ^^^^^
+    for(int ii = 0; ii < t; ii = ii + block_t){
+        for(int jj = 0; jj < OC; jj = jj + block_oc){
+            for(int kk =0; kk < C; kk = kk + block_c){
+                for(int i = ii; i < min(T,block_t+i); i = i +1){
+                    for(int j = jj ; j < min(OC,block_oc+j); j = j + 1){
+                        r = 0;
+                        for(int k = kk; k < min(C,k+blcok_c); k = k + 1){
+                            r = r + inp[i][k] * weight[k][j] + bias[k];
+                        }
+                    }
+                }
+            }  
+        }
+    }
 }
