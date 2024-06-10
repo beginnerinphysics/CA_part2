03189075a3b425067228e6d87c0869831ecd5e39
tfworld <Gieroha90021@gmail.com>
2024-06-10 02:16:01 +0000

    Build residual.cpp
    I build it with just addition in this, it's sort of weird because
    it's a bit simple to build.
    Maybe I should check the normalize problem in output inp1 inp2.

--- residual.cpp
+++ residual.cpp
@@ -1,5 +1,8 @@
 void residual_forward_cpu(float* out, const float* inp1, const float* inp2, int N) {
-    // vvvvv Put your code here vvvvv
-
-    // ^^^^^ Put your code here ^^^^^
+//normalize之前的值和attention之後的值相加, N是 T*C
+//inp1是T*C, inp2應該也是T*C, 也許是因為是pointer所以可以用一維的方式實現, 原因待確認
+//通常來說, residual完之後會在次normalize, attention完之後也會, 但最先embedding完的x不會
+    for (int i = 0; i < N; i++) {
+        out[i] = inp1[i] + inp2[i];
+    }
 }
