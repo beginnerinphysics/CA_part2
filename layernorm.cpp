bd98e2f34c59c99b33a2806b42686842fb80ed80
tfworld <Gieroha90021@gmail.com>
2024-06-10 02:08:11 +0000

    Fix most of bugs in layernorm.cpp
    1.I finall understand the meaning of gamma and beta in the formula
    is weight abd bias so I fixed by replacing it.
    2.I find bias is the same in the same token, so I fix it.
    3.I find mu should be devide by not channel but C, and out/inp
    index should be token not t, so I fixed.
    4.I get the meaning about epsilon in formula so I use 1e-5 for this
    and fix it
    5.I find sigma and be write more comprehensive, so I alter it..

--- layernorm.cpp
+++ layernorm.cpp
@@ -1,26 +1,26 @@
 #include "math.h"
 #include "layernorm.h"
 using namespace std;
+const float EPSILON = 1e-5;
 void layernorm_forward_cpu(float* out, const float* inp, const float* weight, const float* bias,
-                       int T, int C) {
+                       int T, int C) {//公式內的epsilon是為了避免除以零用的, 通常值都很小
     for (int token = 0; token < T; token++) {// separate token
-        float mu = 0,sqrsigma = 0;
+        float mu = 0,sigma = 0;
         
         for (int channel = 0; channel < C; channel++) {// cal mu
             mu += inp[token * C + channel];
         }
-        mu /= channel;
+        mu /= C;
         
         
         for (int channel = 0; channel < C; channel++) {// cal sigma
             float val = inp[t * C + channel] - mu;
-            sqrsigma += val * val;
+            sigma += val * val;
         }
 //        sigma = sqrt(sigma / C); //cal this when we need a number which is not squre sigma
-        sqrsigma /= C;
-        
+        sigma = sqrt(sigma / C + EPSILON)
         for (int channel = 0; channel < C; channel++) {///needfix: gamma and beta not define
-            out[t * C + channel] = (inp[t * C + channel] - mu) / sigma * gamma[c] + beta[c];
+            out[token * C + channel] = (inp[token * C + channel] - mu) / sigma * weight[c] + bias[c];
         }
     }
 }
