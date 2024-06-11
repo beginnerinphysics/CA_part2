baa3a6d1b1b2585faa76f277f9242e4738f804b4
tfworld <eroha90021@gmail.com>
2024-06-11 06:42:10 +0000

    Fix matmul.cpp some functionality syntax
    Bias should be add just once, so I move this addition from r to
    out.And due to the array is one dimension, so I change the imp-
    lementation to fit in 1D.

--- matmul.cpp
+++ matmul.cpp
@@ -11,16 +11,16 @@ void matmul_forward_cpu(float* out,
                     int T, int C, int OC) {
     int iter=0 ;              
     for(int ocend=0; ocend!=OC;ocend=ocend+C){                  
-        for(int ii = 0; ii < t; ii = ii + block_t){
+        for(int ii = 0; ii < T; ii = ii + block_t){
             for(int jj = 0; jj < C; jj = jj + block_c){
                 for(int kk =0; kk < C; kk = kk+block_oc){
                     for(int i = ii; i < T&& i<block_t+ii; i = i +1){
                         for(int j = jj ; j < OC&&j<block_c+jj; j = j + 1){
                             float r = 0;
-                            for(int k = kk; k <C && k<kk+blcok_oc; k = k + 1){
-                                r = r + inp[i][k] * weight[k][j+ocend] + bias[i+iter*T];//這樣寫的話應該就是QKV獨立了, 但還需驗證
+                            for(int k = kk; k <C && k<kk+block_oc; k = k + 1){
+                                r = r + inp[i*C+k] * weight[k*C+j+ocend*T] ;//這樣寫的話應該就是QKV獨立了, 但還需驗證
                             }
-                            out[i][j+ocend] = r;
+                            out[i*C+j+ocend*T] = r + bias[i+iter*T];
                         }
                     }
                 }  
