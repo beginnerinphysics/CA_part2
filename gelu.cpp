8bd4bb7a96abc9f3c50a5c4c16f3f5466b730a20
tfworld <eroha90021@gmail.com>
2024-06-13 08:16:06 +0000

    Refine gelu.cpp
    I find initial file has some define parameter. So I put it back.

--- gelu.cpp
+++ gelu.cpp
@@ -1,13 +1,13 @@
 #include <cmath>
 //float sigmoid(float x) { return 1.0 / (1.0 + exp(-x)); }
-
+#define GELU_SCALING_FACTOR sqrtf(2.0f / M_PI)
 void gelu_forward_cpu(float* out, const float* inp, int N) {
     
     for (int i = 0; i < N; ++i) {
         float x = inp[i];//not sure if just use inp[i]will make it slower
         
         //first version
-        out[i] = 0.5 * x * (1+tanh(sqrt(2/M_PI) * (x + 0.044715 * pow(x,3))));
+        out[i] = 0.5 * x * (1+tanh(GELU_SCALING_FACTOR * (x + 0.044715 * pow(x,3))));
         //second version, seems lead little miss
         //out[i] = x * sigmoid(1.702 * x);
     }
