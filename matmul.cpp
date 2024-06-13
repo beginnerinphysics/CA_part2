2add3771de981a99bc92184b65a0ac458dedc171
tfworld <eroha90021@gmail.com>
2024-06-13 07:02:15 +0000

    Done matmul.cpp with not refine locality
    Maybe I can use block to refine performance if I still have time
    to the deadline

--- matmul.cpp
+++ matmul.cpp
@@ -2,30 +2,30 @@
 #include <cstddef>
 #include <iostream>
 #include <stdio.h>
-#define block_t 4
-#define block_oc 4
-#define block_c 4//初版完成之後有時間需要回來研究修改此block, 已達到更好的locality
+
 using namespace std;
+
 void matmul_forward_cpu(float* out,
                     const float* inp, const float* weight, const float* bias,
                     int T, int C, int OC) {
-    int iter=0 ;              
-    for(int ocend=0; ocend!=OC;ocend=ocend+C){                  
-        for(int ii = 0; ii < T; ii = ii + block_t){
-            for(int jj = 0; jj < C; jj = jj + block_c){
-                for(int kk =0; kk < C; kk = kk+block_oc){
-                    for(int i = ii; i < T&& i<block_t+ii; i = i +1){
-                        for(int j = jj ; j < OC&&j<block_c+jj; j = j + 1){
-                            float r = 0;
-                            for(int k = kk; k <C && k<kk+block_oc; k = k + 1){
-                                r = r + inp[i*C+k] * weight[k*C+j+ocend*T] ;//這樣寫的話應該就是QKV獨立了, 但還需驗證
-                            }
-                            out[i*C+j+ocend*T] = r + bias[i+iter*T];
-                        }
-                    }
-                }  
+    for (int qkv = 0; qkv < OC; qkv += C) {
+        const float* qkv_weight = weight + qkv*C ;
+  //      const float* qkv_bias = bias + qkv*C ;
+        float* qkv_out = out + qkv; // 修正 qkv_out 的計算方式
+
+        for (int i = 0; i < T; i++) {
+            for (int j = 0; j < C; j++) {
+                float r = 0;
+                for (int k = 0; k < C; k++) {
+                    r += inp[i * C + k] * qkv_weight[k + j*C];
+                }
+                qkv_out[i * OC + j] = r + bias[qkv + j]; // 修正 qkv_out 的存取索引
             }
         }
-        iter = iter + 1;
     }
 }
+
+//已知換成1Darray QKV是交錯的
+//已知bias的內部參數應該放i才是rowwise,可是放j才符合3*C的malloc, 實驗顯示要使用j
+//另外有一個大重點:OC不一定只有qkv三個值, 也就是OC不一定是3倍的C
+//注意矩陣的乘法a*b時, b的傳入是已經轉置了
