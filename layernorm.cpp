438e9e3461925688daa4a17ff7b344d577c53c68
little learner <58068903+beginnerinphysics@users.noreply.github.com>
2024-06-15 23:34:52 +0800

    Refine unfinished layernorm.cpp compare 19 26 line combine

--- layernorm.cpp
+++ layernorm.cpp
@@ -3,24 +3,27 @@
 using namespace std;
 const float EPSILON = 1e-5;
 void layernorm_forward_cpu(float* out, const float* inp, const float* weight, const float* bias,
-                       int T, int C) {//公式內的epsilon是為了避免除以零用的, 通常值都很小
-    for (int token = 0; token < T; token++) {// separate token
-        float mu = 0,sigma = 0;
+                       int T, int C) {
+    float mu[T] = {0}
+    for (int token = 0; token < T; token++) {
+        float mu[T] = 0;
+        float sig = 0;
         
-        for (int channel = 0; channel < C; channel++) {// cal mu
-            mu += inp[token * C + channel];
-        }
-        mu /= C;
-        
-        
-        for (int channel = 0; channel < C; channel++) {// cal sigma
-            float val = inp[token * C + channel] - mu;
-            sigma += val * val;
-        }
-//        sigma = sqrt(sigma / C); //cal this when we need a number which is not squre sigma
-        sigma = sqrt(sigma / C + EPSILON);
         for (int channel = 0; channel < C; channel++) {
-            out[token * C + channel] = (inp[token * C + channel] - mu) / sigma * weight[channel] + bias[channel];
+            mu[T] += inp[token * C + channel];
+        }
+        mu[T] /= C;
+        
+        
+        for (int channel = 0; channel < C; channel++) {
+            float val = inp[token * C + channel] - mu[T];
+            sig += val * val;
+        }
+
+        sig = sqrt(sig / C + EPSILON);
+        for (int channel = 0; channel < C; channel++) {
+          
+            out[token * C + channel] = (inp[token * C + channel] - mu[T]) / sig * weight[channel] + bias[channel];
         }
     }
 }
