713456355fa5341b3a6ce46a6b9a04dd0e13d1aa
little learner <58068903+beginnerinphysics@users.noreply.github.com>
2024-06-15 23:38:24 +0800

    Refine matmul.cpp

--- matmul.cpp
+++ matmul.cpp
@@ -3,34 +3,22 @@
 #include <iostream>
 #include <stdio.h>
 
-using namespace std;//如果最後沒用到可以刪掉
+using namespace std;
 
 void matmul_forward_cpu(float* out,
                     const float* inp, const float* weight, const float* bias,
                     int T, int C, int OC) {
     for (int qkv = 0; qkv < OC; qkv += C) {
-
-        const float* qkv_weight = weight + qkv*C ;//如果QKV weight是一個矩陣,串連,根據p20頁的說法, 且考慮轉置的效果, 所以QKV造成的影響要用轉置後的W去想
-
-
-        float* qkv_out = out + qkv; // 如果out是一個矩陣,串連
-        
-
+        float* qkv_weight = weight + qkv*C 
+        float* qkv_out = out + qkv;
         for (int i = 0; i < T; i++) {
             for (int j = 0; j < min(C, OC - qkv); j++) {
                 float r = 0;
                 for (int k = 0; k < C; k++) {
-                    r += inp[i * C + k] * qkv_weight[k + j*C];//此種方式至的output是對的, 暗指已經轉置過了
-
+                    r += inp[i * C + k] * qkv_weight[k + j*C];
                 }
-            //    qkv_out[i * C + j] = r + bias[qkv + j]; // 如果out是三個矩陣, 完全獨立
-                qkv_out[i * OC + j] = r + bias[qkv + j]; // 如果out是一個矩陣, 串連
+                qkv_out[i * OC + j] = r + bias[qkv + j]; 
             }
         }
     }
 }
-
-//已知換成1Darray QKV是交錯的
-//已知bias的內部參數應該放i才是rowwise,可是放j才符合3*C的malloc, 實驗顯示要使用j, 也許跟轉置有關
-//另外有一個大重點:OC不一定只有qkv三個值, 也就是OC不一定是3倍的C
-//注意矩陣的乘法a*b時, b的傳入是已經轉置了
