0772805628c1bd7c8503d8411dac3c0f970ef755
little learner <58068903+beginnerinphysics@users.noreply.github.com>
2024-06-15 21:11:56 +0800

    Refine gelu.cpp with some trick
    Try use x_squared to speed up, pre calculate factor to speed up,
    not sure if not use x and direct inp[i] yield better performance, so
    I don't change this. Try merge one x outside hope it make better
    performance

--- gelu.cpp
+++ gelu.cpp
@@ -1,13 +1,16 @@
 #include <cmath>
 //float sigmoid(float x) { return 1.0 / (1.0 + exp(-x)); }
 #define GELU_SCALING_FACTOR sqrtf(2.0f / M_PI)
+float factor = GELU_SCALING_FACTOR;
 void gelu_forward_cpu(float* out, const float* inp, int N) {
     
     for (int i = 0; i < N; ++i) {
         float x = inp[i];//not sure if just use inp[i]will make it slower
         
         //first version
-        out[i] = 0.5 * x * (1+tanh(GELU_SCALING_FACTOR * (x + 0.044715 * pow(x,3))));
+        //o = 0.5 * x * (1+tanh(factor * (x + 0.044715 * x * x * x)));
+		x_squared = x * x;
+		out[i] = 0.5f * x * (1+tanhf(factor * (x *( 1 + 0.044715f * x_squared))));
         //second version, seems lead little miss
         //out[i] = x * sigmoid(1.702 * x);
     }
