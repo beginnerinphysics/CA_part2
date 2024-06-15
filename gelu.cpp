de62f98c32f52b6d532f16d3a07f6b89c3df04bc
little learner <58068903+beginnerinphysics@users.noreply.github.com>
2024-06-15 23:30:02 +0800

    Refine gelu.cpp

--- gelu.cpp
+++ gelu.cpp
@@ -1,14 +1,8 @@
 #include <cmath>
-//float sigmoid(float x) { return 1.0 / (1.0 + exp(-x)); }
 #define GELU_SCALING_FACTOR sqrtf(2.0f / M_PI)
 void gelu_forward_cpu(float* out, const float* inp, int N) {
-    
     for (int i = 0; i < N; ++i) {
-        float x = inp[i];//not sure if just use inp[i]will make it slower
-        
-        //first version
+        float x = inp[i];
         out[i] = 0.5 * x * (1+tanh(GELU_SCALING_FACTOR * (x + 0.044715 * pow(x,3))));
-        //second version, seems lead little miss
-        //out[i] = x * sigmoid(1.702 * x);
     }
 }
