348d56c4ef2c091fa88de49dc4cd5c9999f3b103
little learner <58068903+beginnerinphysics@users.noreply.github.com>
2024-06-15 22:18:18 +0800

    Refine layernorm.cpp

--- layernorm.cpp
+++ layernorm.cpp
@@ -6,21 +6,21 @@ void layernorm_forward_cpu(float* out, const float* inp, const float* weight, co
                        int T, int C) {//公式內的epsilon是為了避免除以零用的, 通常值都很小
     for (int token = 0; token < T; token++) {// separate token
         float mu = 0,sigma = 0;
-        
+        const int TOKENmulC = token * C;
         for (int channel = 0; channel < C; channel++) {// cal mu
-            mu += inp[token * C + channel];
+            mu += inp[TOKENmulC + channel];
         }
         mu /= C;
         
         
         for (int channel = 0; channel < C; channel++) {// cal sigma
-            float val = inp[token * C + channel] - mu;
+            float val = inp[TOKENmulC + channel] - mu;
             sigma += val * val;
         }
 //        sigma = sqrt(sigma / C); //cal this when we need a number which is not squre sigma
         sigma = sqrt(sigma / C + EPSILON);
         for (int channel = 0; channel < C; channel++) {
-            out[token * C + channel] = (inp[token * C + channel] - mu) / sigma * weight[channel] + bias[channel];
+            out[TOKENmulC + channel] = (inp[TOKENmulC + channel] - mu) / sigma * weight[channel] + bias[channel];
         }
     }
 }
