62145de0119bde12a84883bdd6ec320e3ac27690
little learner <58068903+beginnerinphysics@users.noreply.github.com>
2024-06-15 20:40:34 +0800

    Refine attention.cpp with pointer and memory malloc reduction
    Change inp_qkv to int_q, k, and v, and change rsoftmax inside iter
    loop i make malloc less, make r malloc less, adpot the code with no
    maxvalue in softmax to reduce unnecessary calculate.

--- attention.cpp
+++ attention.cpp
@@ -7,35 +7,29 @@ void attention_forward_cpu(float* out, const float* inp, int T, int C, int NH) {
 using namespace std;
     int H = C / NH;
     for(int head = 0;head < NH;head = head + 1){
-        const float *inp_qkv =  inp + head * H;
-        float *out_qkv = out + head*H;
-        float r[T][T] = {0};
-        float rsoftmax[T] = {0};
+        const float *inp_q =  inp + head * H;
+		const float *inp_k =  inp + C + head * H;
+		const float *inp_v =  inp + 2 * C + head * H;
+        float *out_qkv = out + head * H;
         for(int i = 0; i < T; i = i + 1){
-            float max_val = 0;
+            float rsoftmax = 0;
+			float r[T] = {0};
             for(int j = 0; j < T; j = j + 1){
                 for(int k = 0;k < H;k = k + 1){
-                    r[i][j] = r[i][j] + inp_qkv[i * C * 3 + k] * inp_qkv[C + j * C * 3 + k];//如果用3就無法scale up, 希望不是大問題, 另外要注意轉置的問題, 畫圖會幫助理解
+                    r[j] = r[j] + inp_q[i * C * 3 + k] * inp_k[j * C * 3 + k];//如果用3就無法scale up, 希望不是大問題, 另外要注意轉置的問題, 畫圖會幫助理解
                 }
-                r[i][j] = r[i][j] / sqrt(H);//H is 8
-                max_val = fmax(max_val, r[i][j]);
-                
-            }
+                r[j] = exp(r[j] / sqrt(H));//H is 8
+                rsoftmax = rsoftmax + r[j];
+            }//Q K multiply done already
             for(int j = 0; j < T; j = j + 1){
-                r[i][j] = exp(r[i][j] - max_val);
-                rsoftmax[i] = rsoftmax[i] + r[i][j];
-            }//Q and K have done already
-            for(int j = 0; j < T; j = j + 1){
-                r[i][j] = r[i][j] / rsoftmax[i];
-            }
+				float o = 0;
+				for(int h = 0;h < T;h = h + 1){
+					r[j] = r[j] / rsoftmax;
+					o = o + r[j] * inp_v[j + h * C * 3];//記得output只有T*C的大小
+				}
+				out_qkv[i * C + j] = o;
+			}
             
-            for(int j = 0; j < H; j = j + 1){
-                float o = 0;
-                for(int h = 0;h < T;h = h + 1){
-                    o = o + r[i][h] * inp_qkv[2 * C + j + h * C * 3];//記得output只有T*C的大小
-                }
-                out_qkv[i * C + j] = o;
-            }
         }
         
     }
